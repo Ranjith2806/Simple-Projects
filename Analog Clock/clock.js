8423514ffebce98f93ffef5beed6b6529239b618
Ranjith2806 <ranjithkumars2806@gmail.com>
2022-09-27 23:48:36 +0530

    js commit 2

--- Analog Clock/clock.js
+++ Analog Clock/clock.js
@@ -3,4 +3,11 @@ const hour = document.querySelector(".hour");
 const min = document.querySelector(".min");
 const sec = document.querySelector(".sec");
 
-const setClock
+const setClock = () => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hour.style.transform = 'rotateZ(${hh + mm / 12}deg)';
+}
